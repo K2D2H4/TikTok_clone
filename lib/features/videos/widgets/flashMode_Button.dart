96b2e61ab15084dd12616ae8f29223ca9c67b589
KDH <daehyun9714@gmail.com>
2023-12-13 17:30:37 +0900

    Update navigator.

--- lib/features/videos/widgets/flashMode_Button.dart
+++ lib/features/videos/widgets/flashMode_Button.dart
@@ -2,23 +2,23 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class FlashModeButton extends StatefulWidget {
-  final FlashMode flashMode;
   final FlashMode newFlashMode;
   final IconData icon;
+  final CameraController cameraController;
 
   const FlashModeButton(
       {super.key,
-      required this.flashMode,
       required this.icon,
-      required this.newFlashMode});
+      required this.newFlashMode,
+      required this.cameraController});
 
   @override
   State<FlashModeButton> createState() => _FlashModeButtonState();
 }
 
 class _FlashModeButtonState extends State<FlashModeButton> {
-  late CameraController _cameraController;
-  late FlashMode _flashMode = widget.flashMode;
+  late CameraController _cameraController = widget.cameraController;
+  late FlashMode _flashMode = _cameraController.value.flashMode;
 
   Future<void> _setFlashMode(FlashMode newFlashMode) async {
     await _cameraController.setFlashMode(newFlashMode);
@@ -30,7 +30,7 @@ class _FlashModeButtonState extends State<FlashModeButton> {
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: () => _setFlashMode(widget.newFlashMode),
-      color: _flashMode == widget.flashMode ? Colors.amber : Colors.white,
+      color: _flashMode == widget.newFlashMode ? Colors.amber : Colors.white,
       icon: Icon(
         widget.icon,
       ),
